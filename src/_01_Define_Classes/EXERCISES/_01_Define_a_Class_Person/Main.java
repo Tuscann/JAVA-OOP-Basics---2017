6d6e7c4b182211b6738b5bd000550d1a2d2315c4
Unknown <fbinnzhivko@gmail.com>
2017-11-04 15:29:34 +0200

    Auto stash before merge of "master" and "origin/master"

--- src/_01_Define_Classes/EXERCISES/_01_Define_a_Class_Person/Main.java
+++ src/_01_Define_Classes/EXERCISES/_01_Define_a_Class_Person/Main.java
@@ -2,11 +2,10 @@ package _01_Define_Classes.EXERCISES._01_Define_a_Class_Person;
 
 import java.lang.reflect.Field;
 
-
 public class Main {
     public static void main(String[] args) {
         Class person = Person.class;
-        Field[] filds = person.getDeclaredFields();
-        System.out.println(filds.length);
+        Field[] fields = person.getDeclaredFields();
+        System.out.println(fields.length);
     }
 }
